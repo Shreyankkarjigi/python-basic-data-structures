9b04b626ea5a955b95166b8aa9e490bfa65084b1
Shreyankkarjigi <androgeek123@gmail.com>
2020-08-15 14:30:37 +0530

    Update Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -16,52 +16,40 @@ operations on tuples:
 5.concat
 6.Iterable
 7.Faster than lists
-
 '''
 
 number=(1,2,3,4,5)
 char=("a","b","c")
 both=(1,2,3,"a","b")
-#delete entire tuple
-
-
 
 '''
-
-#to know index of some element 
-
+#To know index of some element 
 #enumerate(to find index of element in iterable object)
-
 for element in enumerate(char):
     print(element)
-
 
 #allows duplicates
 numbers=(1,1,"a","a")
 '''
-
 #concat tuples
-'''
-for i in number:
-    print(i)
-
+add=number+char
+print(add)
 
 #slicing
 #rule of index
 #[index:index-1]
-'''
-num=(1,2,4,5)
 
+num=(1,2,4,5)
 print(num[0:3])#index
 print(num[1])#single element acess
 print(num[-1])#last element
 print(num[::-1])#reverse tuples
 
-t=(1,2,3,4,4,5,56,6,6)
-print(t[::-5])
 #striding
 #[start:end:steps]
 t1=(1,2,3,4,5,6,7,8)
 #print every second element
 print(t1[1:7:2])
+t=(1,2,3,4,4,5,56,6,6)
+print(t[::-5])
 
